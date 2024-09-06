ef1df5a9b97aa28c8e1109f943ec25559dda6289
Mar 🌊 <mariajara929@gmail.com>
2024-09-06 21:25:50 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,103 +6,121 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let syllables1 = [
+    "ba",
+    "ca",
+    "da",
+    "fa",
+    "ga",
+    "ha",
+    "ja",
+    "ka",
+    "la",
+    "ma",
+    "na",
+    "pa",
+    "ra",
+    "sa",
+    "ta",
+    "va",
+    "wa",
+    "ya",
+    "za",
+    "sha",
+    "cha",
+    "tha",
+    "bra",
+    "kra",
+    "tra",
+    "pla",
+    "fra",
+    "gra",
+    "sma",
+    "spa"
+  ];
+  let syllables2 = [
+    "ab",
+    "ac",
+    "ad",
+    "af",
+    "ag",
+    "eb",
+    "ec",
+    "ed",
+    "ef",
+    "eg",
+    "ib",
+    "ic",
+    "id",
+    "if",
+    "ig",
+    "ob",
+    "oc",
+    "od",
+    "of",
+    "og",
+    "ub",
+    "uc",
+    "ud",
+    "uf",
+    "ug",
+    "ib",
+    "im",
+    "il",
+    "in",
+    "ir"
+  ];
+  let syllables3 = [
+    "ast",
+    "end",
+    "ist",
+    "int",
+    "ant",
+    "ond",
+    "ing",
+    "est",
+    "ent",
+    "ang",
+    "ust",
+    "ond",
+    "ist",
+    "unt",
+    "ong",
+    "elt",
+    "ult",
+    "ost",
+    "ift",
+    "ilt",
+    "and",
+    "apt",
+    "ext",
+    "oft",
+    "olt",
+    "ind",
+    "ect",
+    "apt",
+    "est",
+    "ump"
+  ];
 
-let syllables1 = [
-  "ba",
-  "ca",
-  "da",
-  "fa",
-  "ga",
-  "ha",
-  "ja",
-  "ka",
-  "la",
-  "ma",
-  "na",
-  "pa",
-  "ra",
-  "sa",
-  "ta",
-  "va",
-  "wa",
-  "ya",
-  "za",
-  "sha",
-  "cha",
-  "tha",
-  "bra",
-  "kra",
-  "tra",
-  "pla",
-  "fra",
-  "gra",
-  "sma",
-  "spa"
-];
-const syllables2 = [
-  "ab",
-  "ac",
-  "ad",
-  "af",
-  "ag",
-  "eb",
-  "ec",
-  "ed",
-  "ef",
-  "eg",
-  "ib",
-  "ic",
-  "id",
-  "if",
-  "ig",
-  "ob",
-  "oc",
-  "od",
-  "of",
-  "og",
-  "ub",
-  "uc",
-  "ud",
-  "uf",
-  "ug",
-  "ib",
-  "im",
-  "il",
-  "in",
-  "ir"
-];
-const syllables3 = [
-  "ast",
-  "end",
-  "ist",
-  "int",
-  "ant",
-  "ond",
-  "ing",
-  "est",
-  "ent",
-  "ang",
-  "ust",
-  "ond",
-  "ist",
-  "unt",
-  "ong",
-  "elt",
-  "ult",
-  "ost",
-  "ift",
-  "ilt",
-  "and",
-  "apt",
-  "ext",
-  "oft",
-  "olt",
-  "ind",
-  "ect",
-  "apt",
-  "est",
-  "ump"
-];
+  // Function to generate a random word from the syllables
+  function generateRandomWord() {
+    let randomSyllable1 =
+      syllables1[Math.floor(Math.random() * syllables1.length)];
+    let randomSyllable2 =
+      syllables2[Math.floor(Math.random() * syllables2.length)];
+    let randomSyllable3 =
+      syllables3[Math.floor(Math.random() * syllables3.length)];
+
+    // Combine the syllables
+    const word = randomSyllable1 + randomSyllable2 + randomSyllable3;
+
+    // Display the generated word
+    document.getElementById("generatedWord").innerText = word;
+  }
+
+  // Event listener to the button
+  document
+    .getElementById("generateButton")
+    .addEventListener("click", generateRandomWord);
+};
